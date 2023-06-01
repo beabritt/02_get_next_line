ae2316101e44e45d0c9a98e0e41a01e37db1a33e
Beatriz Camino Marquez <becamino@c1r15s4.42madrid.com>
2023-06-01 19:59:21 +0200

    gnl

--- prueba gnl/gnl.c
+++ prueba gnl/gnl.c
@@ -6,7 +6,7 @@
 /*   By: becamino <becamino@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:44:23 by becamino          #+#    #+#             */
-/*   Updated: 2023/06/01 19:28:25 by becamino         ###   ########.fr       */
+/*   Updated: 2023/06/01 19:56:19 by becamino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,8 @@ char	*ft_cut(char *stash)
 	res[i] = stash[i];
 	i++;
 	res[i] = '\0';
-	printf("%s\n", res);
-	printf("%d\n", i);
 	stash = ft_save (stash, len, i);
+	printf("stash en ft_cut: %s\n", stash);
 	return(res);
 }
 
@@ -76,17 +75,20 @@ char	*ft_read(int fd, char *stash)
 		readd[BUFFER_SIZE] = '\0';
 		if (stash)
 		{
+			printf("lleno\n");
 			aux = ft_strjoin_gnl(stash, readd);
 			free (stash);
 			stash = aux;
 		}
 		else
+			printf("vacio\n");
 			stash = ft_strjoin_gnl(stash, readd);
 			y = ft_strchr_gnl(stash, 10);
 			//printf("%d\n", y);
 		if (y == 1)
 		{
 			line = ft_cut(stash);
+			printf("stash y: %s\n", stash);
 			return(line);
 		}
 	}
@@ -110,11 +112,13 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 
+	printf("stash: %s\n", stash);
 	str = ft_read(fd, stash);
 	
 	/* printf("\n %s",str);
 	printf("\n %s", stash); */
 
+	printf("stash fnl: %s\n", stash);
 	return(str);	
 }
 
--- prueba gnl/maingnl.c
+++ prueba gnl/maingnl.c
@@ -6,7 +6,7 @@
 /*   By: becamino <becamino@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:31:12 by becamino          #+#    #+#             */
-/*   Updated: 2023/06/01 19:29:21 by becamino         ###   ########.fr       */
+/*   Updated: 2023/06/01 19:42:41 by becamino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int main(void)
     fd = open("hola.txt", O_RDONLY);
     if (!fd)
         return (0);
-	while (x < 2)
+	while (x < 1)
 	{
    		line = get_next_line(fd);
 		free(line);
